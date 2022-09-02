10ed6ea90813308f405a1262ed6fbb2631f00423
PD-1975 <109069988+PD-1975@users.noreply.github.com>
2022-09-02 11:19:29 +0200

    Add files via upload

---
+++ Javascript-hello/index.js
@@ -0,0 +1,19 @@
+console.log("Hello Winc Academy");
+let name = "Paul";
+console.log(name);
+// niet mogelijk om een variabel in een if te maken
+console.log(4 + 4);
+console.log("4" + "4");
+let y = 10;
+let a = y + 2;
+let b = y - 2;
+let c = y * 2;
+let d = y / 2;
+let e = y % 2;
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+let age = "zevenenveertig";
+console.log(age);
