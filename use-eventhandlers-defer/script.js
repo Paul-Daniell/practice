fdc6ab54d6376afb58a18eca61cb7bc30a9cbf7a
PD-1975 <vanschevikhoven.pd@gmail.com>
2022-09-09 16:01:07 +0200

    aangepast

--- use-eventhandlers-defer/script.js
+++ use-eventhandlers-defer/script.js
@@ -2,13 +2,16 @@ mybutton.addEventListener("click", function () {
   alert("change the blue to red background werkt niet!!!");
 });
 
-const changeClassBlueBackground = function () {
+const toggleColor = function () {
   const bodyElement = document.body;
-  bodyElement.classList.add("red-background");
+  bodyElement.classList.toggle("red-background");
 };
 const attachEventToChangeColorButton = function () {
   const changeColorButton = document.getElementById("change-background-button");
   changeColorButton.addEventListener("click", function () {
-    changeClassBlueBackground();
+    toggleColor();
   });
 };
+
+toggleColor();
+attachEventToChangeColorButton();
