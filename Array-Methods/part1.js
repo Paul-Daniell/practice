cef6b979b824cccbde8b02b60e9a4eef91a7efc3
PD-1975 <vanschevikhoven.pd@gmail.com>
2022-09-29 10:20:30 +0200

    first commit

---
+++ Array-Methods/part1.js
@@ -0,0 +1,58 @@
+// const addTheWordCool = function (array) {
+//   array.push("cool");
+//   return array;
+// };
+
+// console.log("Add cool:", addTheWordCool(["nice", "awesome", "tof"]));
+
+// const amountOfElementsInArray = function (array) {
+//   return array.length;
+// };
+
+// console.log(amountOfElementsInArray(["apples", "pears", "lemons"]));
+
+// const selectBelgiumFromBenelux = function (array) {
+//   return array[0];
+// };
+// console.log(selectBelgiumFromBenelux(["Belgium", "Netherlands", "Luxembourg"]));
+
+// const lastElementInArray = function (array) {
+//   return array[3];
+// };
+// console.log(lastElementInArray(["Hare", "Guinea pig", "Chicken", "Turtle"]));
+
+// const lastElementInArray = function (array) {
+//   return array.pop();
+// };
+// console.log(lastElementInArray(["Hare", "Guinea pig", "Chicken", "Turtle"]));
+
+// const presidents = ["Trump", "Obama", "Bush", "Clinton"];
+
+// const impeachTrumpSlice = function (array) {
+//   return presidents.slice(1);
+// };
+// const impeachTrumpSplice = function (array) {
+//   return presidents.splice(1);
+// };
+
+// console.log(impeachTrumpSlice(presidents)); // ["Obama", "Bush", "Clinton"]
+// console.log(impeachTrumpSplice(presidents)); // ["Obama", "Bush", "Clinton"]
+
+// const stringsTogether = function (array) {
+//   return array.join(" ");
+// };
+// console.log(stringsTogether(["Winc", "Academy", "is", "fun", ";-}"]));
+
+// const array1 = [1, 2, 3];
+// const array2 = [4, 5, 6];
+
+// const combineArrays = function (array) {
+//   return array1.concat(array2);
+// };
+// console.log(combineArrays([1, 2, 3], [4, 5, 6]));
+
+// kortere versie!
+// const combineArrays = function (array1, array2) {
+//   return array1.concat(array2);
+// };
+// console.log(combineArrays([1, 2, 3], [4, 5, 6]));
